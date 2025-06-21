f3ae39e8fbda86c7ec030586baa5b1674e21a3b4
Jovan Dosanjh <jovandosanjh@icloud.com>
2025-06-21 12:28:15 -0400

    first change

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome pussy!</ThemedText>
+        <ThemedText type="title">Welcome pussy bomboross!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
