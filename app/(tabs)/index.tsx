2a48e5e349e7ac2c141cb7a280d147eca768df2c
Jovan Dosanjh <jovandosanjh@icloud.com>
2025-06-21 12:58:50 -0400

    dfijgidj

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
-const index = () => {
+const app = () => {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Coffee house</Text>
     </View>
   )
 }
 
-export default index
+export default app
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        flexDirection: 'column',
+    },
+    text: {
+        color: 'black',
+        fontSize: 42,
+        fontWeight: 'bold',
+        textAlign: 'center',
+
+    }
+})
